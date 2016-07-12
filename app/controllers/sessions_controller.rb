1dda8e92ccd384a0d908a179845cdb431e1b44c2
Brian Sayler <sayler.b@gmail.com>
2016-07-12 15:55:51 -0600

    edit sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,23 @@ class SessionsController < ApplicationController
     code = params["code"]
 
     uri = URI('https://www.reddit.com/api/v1/access_token')
-    req = Net::HTTP::Post.new(uri)
+    #data = "grant_type=authorization_code&code=#{code}&redirect_uri=http://localhost:3000/auth/reddit/callback"
+
+    data = { grant_type: "authorization_code", 
+             code: code, 
+             redirect_uri: "http://localhost:3000/auth/reddit/callback"
+           }
+
+    req = Net::HTTP::Post.new(uri, data)
+
     req.basic_auth ENV["REDDIT_KEY"], ENV["REDDIT_SECRET"]
-    req.body = "grant_type=authorization_code&code=#{code}&redirect_uri=http://localhost:3000/auth/reddit/callback"
+
     res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
       http.request(req)
     end
     res.body
 
+    binding.pry
     render text: res.body.inspect
 
   end
