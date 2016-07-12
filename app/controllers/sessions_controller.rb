20d61d1cd07b44ae1bb2277c775e218eb8c2fff8
Brian Sayler <sayler.b@gmail.com>
2016-07-12 14:58:56 -0600

    started implementing token request

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,17 @@
 class SessionsController < ApplicationController
   def create
-    render text: request.env["omniauth.auth"].inspect
+    code = params["code"]
+
+    uri = URI('https://www.reddit.com/api/v1/access_token')
+    req = Net::HTTP::Post.new(uri)
+    req.basic_auth ENV["REDDIT_KEY"], ENV["REDDIT_SECRET"]
+    req.body = "grant_type=authorization_code&code=#{code}&redirect_uri=http://localhost:3000/auth/reddit/callback"
+    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
+      http.request(req)
+    end
+    res.body
+
+    render text: res.body.inspect
+
   end
 end
--- config/initializers/omniauth.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :reddit, ENV['REDDIT_KEY'], ENV['REDDIT_SECRET'], 
-    duration: "temporary",
-    scope: "identity,subscribe,vote,submit,history,read,mysubreddits"
-end
-#
-   #  grant_type: "authorization_code",
-   #  redirect_uri: "http://127.0.0.1:3000/auth/reddit/callback"
-
