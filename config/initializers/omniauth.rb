5867856377fc7796743ab503664fa2548b6d8d90
Brian Sayler <sayler.b@gmail.com>
2016-07-11 23:42:28 -0600

    add scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :reddit, ENV['REDDIT_KEY'], ENV['REDDIT_SECRET']
+  provider :reddit, ENV['REDDIT_KEY'], ENV['REDDIT_SECRET'], 
+    scope: "identity,subscribe,vote,submit,history,read,mysubreddits"
 end
+#
+   #  duration: "temporary",
+   #  grant_type: "authorization_code",
+   #  redirect_uri: "http://127.0.0.1:3000/auth/reddit/callback"
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-
   get '/', to: 'home#index'
-
   get '/auth/reddit', as: :reddit_login
-
+  get '/auth/reddit/callback', to: 'sessions#create'
 end
