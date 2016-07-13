1d2f061743f18b93cc49154b62188c3279be5648
Brian Sayler <sayler.b@gmail.com>
2016-07-13 15:37:05 -0600

    make some notes in the controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,15 @@
 class SessionsController < ApplicationController
   def create
     code = params["code"]
-
     uri = URI('https://www.reddit.com/api/v1/access_token')
-    #data = "grant_type=authorization_code&code=#{code}&redirect_uri=http://localhost:3000/auth/reddit/callback"
 
-    data = { grant_type: "authorization_code", 
-             code: code, 
-             redirect_uri: "http://localhost:3000/auth/reddit/callback"
-           }
+    req = Net::HTTP::Post.new(uri)
 
-    req = Net::HTTP::Post.new(uri, data)
+    req.set_form_data({ "grant_type" => "authorization_code", 
+                        "code" => code, 
+                        "redirect_uri" => "http://localhost:3000/auth/reddit/callback",
+                        "Content-Type" => "application/x-www-form-urlencoded"
+                      })
 
     req.basic_auth ENV["REDDIT_KEY"], ENV["REDDIT_SECRET"]
 
@@ -19,8 +18,10 @@ class SessionsController < ApplicationController
     end
     res.body
 
-    binding.pry
-    render text: res.body.inspect
+    render text: res.body.inspect + "Code: " + code
 
+    # Could create a user in the database to store tokens
+    # When they login, it updates that token
+    # Move this code out of the controller
   end
 end
